7d8c031effe3592286c7845fd743c43779bf1682
demonflowne <ibuemmanuel60@gmail.com>
2024-09-28 10:50:42 +0100

    latest commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import express from "express";
+import express, { Request } from "express";
+import * as http from "http";
+import { type Socket, type ServerOptions } from "socket.io";
+// import { CorsOptions } from "cors";
+// import { Server} from "socket.io";
+import cors from "cors";
+import { Server as SocketIOServer } from "socket.io";
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+const server = http.createServer(app);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = 5002;
 
@@ -11,6 +20,42 @@ process.on("uncaughtException", (err) => {
     process.exit(1);
 });
 
-app.listen(PORT, () => {
+
+
+
+const ioOptions: Partial<ServerOptions> = {
+    cors: {
+      origin: "http://localhost:3000",
+      // origin: "https://workers-chatapp-frontend.vercel.app",
+      methods: ["GET", "POST"],
+    },
+  };
+  
+  const io: SocketIOServer = new SocketIOServer(server, ioOptions);
+  
+  app.get("/", (_req:Request, res) => res.send("Hello World!"));
+  
+  
+  
+  io.on("connection", (socket: Socket) => {  
+    socket.on("disconnect", () => {
+    //   io.emit(
+    //     "get-users",
+    //     online_Users
+    //       .filter((i) => i.socketId !== socket.id)
+    //       .map((j) => ({ email: j.email, socketId: j.socketId }))
+    //   );
+    //   online_Users = online_Users.filter((i) => i.socketId !== socket.id);
+    });
+  });
+  
+
+
+
+
+
+
+
+server.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
 });
